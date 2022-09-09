01a8c1ca6343f560128bd693f7f69ad90d8b6faf
pmemoli <pedromemoli@hotmail.com>
2022-09-08 22:21:10 -0300

    added mate 2

--- src/pages/fisica/Fisica.js
+++ src/pages/fisica/Fisica.js
@@ -22,7 +22,7 @@ function Welcome() {
 export default function Fisica({original, setOriginal}) {
   const [materia, setMateria] = useState('home');
   const materias = [
-    'Fisica 1', 'Matematica 1', 'Laboratorio 1', 'Fisica 2', 'Matematica 3', 'Laboratorio 2', 'Calculo Numerico',
+    'Fisica 1', 'Matematica 1', 'Laboratorio 1', 'Fisica 2', 'Matematica 2', 'Matematica 3', 'Laboratorio 2', 'Calculo Numerico',
     'Fisica 3', 'Matematica 4', 'Laboratorio 3', 'Fisica 4', 'Mecanica Clasica', 'Fisica Teorica 1', 'Laboratorio 4',
     'Fisica Teorica 2', 'Fisica Teorica 3', 'Laboratorio 5', 'Estructura 1', 'Estructura 2', 'Laboratorio 6', 'Laboratorio 7',
     'Estructura 3', 'Estructura 4'
