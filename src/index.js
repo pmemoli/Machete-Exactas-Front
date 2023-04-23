4ede5a219702be87b482e6b767bfe5d20bf99be0
pmemoli <pedromemoli@hotmail.com>
2023-04-22 22:03:32 -0300

    analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-5FYBBBZ3GG");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +12,10 @@ root.render(
       <App style={{height: "100%",}}/>
     </BrowserRouter>
 );
+
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
